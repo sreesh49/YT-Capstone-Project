d8c91524faf08cbd85a424853f9341f65bb5c387
Sreesh <sreesh49@gmail.com>
2026-06-09 21:51:11 +0530

    cicd run5

--- scripts/promote_model.py
+++ scripts/promote_model.py
@@ -5,9 +5,9 @@ import mlflow
 
 def promote_model():
     # Set up DagsHub credentials for MLflow tracking
-    dagshub_token = os.getenv("CAPSTONE_TEST")
+    dagshub_token = os.getenv("DAGSHUB_TOKEN")
     if not dagshub_token:
-        raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
+        raise EnvironmentError("DAGSHUB_TOKEN environment variable is not set")
 
     os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
     os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
